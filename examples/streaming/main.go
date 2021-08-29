1cfaf593e902b14c60212d237492973d9c4e1bbd
fanweixiao <fanweixiao@gmail.com>
2021-08-29 21:12:19 +0800

    add test demostrate stream encode & stream decode

--- examples/streaming/main.go
+++ examples/streaming/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
+	// "io/ioutil"
+	"sync"
+	"time"
 
 	"github.com/yomorun/y3"
 )
@@ -15,10 +17,30 @@ var (
 	TagOfTransactionID byte = 0x01
 )
 
+type p struct {
+	buf      []byte
+	lastRead int
+	wg       *sync.WaitGroup
+}
+
+func (o *p) Read(buf []byte) (int, error) {
+	o.wg.Add(1)
+	defer o.wg.Done()
+	if o.lastRead >= len(o.buf) {
+		return 0, io.EOF
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Printf("(source stream)==>flush:[%# x]\n", o.buf[o.lastRead])
+	copy(buf, []byte{o.buf[o.lastRead]})
+	o.lastRead++
+	return 1, nil
+}
+
 func main() {
+	var wg sync.WaitGroup
 	payloadData := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
-	payloadReader := new(bytes.Buffer)
-	payloadReader.Write(payloadData)
+	payloadReader := &p{buf: payloadData, wg: &wg}
+	// payloadReader.Write(payloadData)
 	// Prepare a DataFrame
 	// DataFrame is combined with a MetaFrame and a PayloadFrame
 	// 1. Prepare MetaFrame
@@ -30,19 +52,44 @@ func main() {
 	payload.SetLength(len(payloadData))
 	payload.SetCarriageReader(payloadReader)
 	// 3. combine to DataFrame
-	// data := new(DataFrame)
-	// data.metaFrame = meta
-	// data.payloadFrame = payload
-
-	// TODO:
 	enc := y3.NewStreamEncoder(TagOfDataFrame)
 	enc.AddPacketBuffer(meta.Encode())
 	// enc.AddStreamPacket(tag, len(payloadData), payloadReader)
 	enc.AddStreamPacket(payload.Sid, payload.length, payload.reader)
-	fmt.Printf("length=%d\n", enc.GetLen())
 
+	// try read
+	fmt.Printf("length=%d\n", enc.GetLen())
 	r := enc.GetReader()
-	buf, err := ioutil.ReadAll(r)
-	fmt.Printf("err=%v\n", err)
-	fmt.Printf("buf=%# x\n", buf)
+
+	// // method 1: try read all
+	// buf, err := ioutil.ReadAll(r)
+	// fmt.Printf("err=%v\n", err)
+	// fmt.Printf("buf=%# x\n", buf)
+
+	// method 2: try read from reader
+	for {
+		sp, err := y3.StreamReadPacket(r)
+		if err != nil {
+			fmt.Printf("err=%v\n", err)
+			break
+		}
+		fmt.Printf(">> tag=%# x\n", sp.Tag)
+		fmt.Printf("length=%d\n", sp.Len)
+		// if sp.Tag == tag {
+		tmp := make([]byte, 1)
+		for {
+			n, err := sp.Val.Read(tmp)
+			if err != nil {
+				if err == io.EOF {
+					fmt.Printf("\t-> %# x\n", tmp[:n])
+				}
+				break
+			}
+			fmt.Printf("\t-> %# x\n", tmp[:n])
+		}
+		// }
+	}
+
+	wg.Wait()
+	fmt.Println("OVER")
 }
